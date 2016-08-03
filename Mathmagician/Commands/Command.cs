70e45002c373a802ab8e07db01b7ae5e0b841d49
MATT BRUTON <the.bruton@gmail.com>
2016-08-03 18:19:25 -0500

    created command class that all commands will inherit from

---
+++ Mathmagician/Commands/Command.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mathmagician.Commands
+{
+    public class Command
+    {
+        public List<int> Numbers = new List<int>();
+        public int QuantityOfNumbers { get; set; }
+    }
+}
