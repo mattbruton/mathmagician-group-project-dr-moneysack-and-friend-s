6b7f505594b0cc5af31e127bc30bde211f14a403
MATT BRUTON <the.bruton@gmail.com>
2016-08-03 00:14:06 -0500

    created methods to return strings for use in app

---
+++ Mathmagician/Dialog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public static class Dialog
+    {
+        public static string Prompt()
+        {
+            return ">";
+        }
+        public static string InitialGreeting()
+        {
+            return "\n\nI am the Mathmagician! I can make all sorts of numbers!\nJust tell me how many numbers you want and what types of numbers to generate.\n";
+        }
+
+        public static string MainMenu()
+        {
+            return "What would you like for me to do?\n\nCreate numbers\nExit\n";
+        }
+
+        public static string ListOfCommands()
+        {
+            return "What type of number or numbers would you like?\n\nIntegers\nPrimes\nFibonacci\nEven\nOdd\n";
+        }
+
+        public static string Whoops()
+        {
+            return "Whoops!";
+        }
+    }
+}
--- Mathmagician/Program.cs
+++ Mathmagician/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Mathmagician
 {
     class Program
     {
+        public static string UserCommand { get; set; }
+        public static int UserCount { get; set; }
         static void Main(string[] args)
         {
+            Console.WriteLine(Dialog.InitialGreeting());
+            Console.WriteLine(Dialog.MainMenu());
+            Console.Write(Dialog.Prompt());
+            Console.ReadLine();
+            Console.WriteLine(Dialog.ListOfCommands());
+            Console.Write(Dialog.Prompt());
+            Console.ReadLine();
+            Console.WriteLine(Dialog.Whoops());
+            Console.WriteLine(UserCommand);
+            Console.WriteLine(UserCount);
+            Console.ReadKey();
         }
     }
 }
