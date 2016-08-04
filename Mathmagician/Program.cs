e14cea0ee63899c27175e649ffcc470af7af025d
MATT BRUTON <the.bruton@gmail.com>
2016-08-04 01:34:38 -0500

    removed junk code from Program.cs

--- Mathmagician/Program.cs
+++ Mathmagician/Program.cs
@@ -8,13 +8,6 @@ namespace Mathmagician
     {
         static void Main(string[] args)
         {
-            Command test = new Command();
-            Console.WriteLine(test.Numbers.Count);
-            test.Numbers.Add(2);
-            test.Numbers.Add(3);
-            test.Numbers.Add(4);
-            Console.WriteLine(test.NumbersToString());
-            Console.ReadKey();
         }
     }
 }
