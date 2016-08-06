b7824864c7b97f84dd683153810e81737cb4a6dd
ZachParris <zacharyjparris@gmail.com>
2016-08-05 19:03:22 -0500

    found error in integer.cs that was preventing project from building

--- Mathmagician/Commands/Integer.cs
+++ Mathmagician/Commands/Integer.cs
@@ -8,7 +8,7 @@ namespace Mathmagician.Commands
 {
     public class Integer : Command
     {
-        public override void GenerateNumbers(int quantityOfNumsToGenerate)
+        public override void CreateNumbers(int quantityOfNumsToGenerate)
         {
             //Create integers to add to Numbers List
             for (int i = 0; i < quantityOfNumsToGenerate; i++)
