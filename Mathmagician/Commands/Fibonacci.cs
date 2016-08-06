d47d9e9d69caf61cccdef845d1badfbeb76948a6
ZachParris <zacharyjparris@gmail.com>
2016-08-06 13:34:25 -0500

    fixed commit error

---
+++ Mathmagician/Commands/Fibonacci.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician.Commands
+{
+    class Fibonacci : Command
+    {
+        public override void CreateNumbers(int quantityOfNumsToCreate)
+        {
+
+        }
+    }
+}
