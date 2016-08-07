57f8d689c2a6374e1d3f8e1f8218b96cc0f846f2
ZachParris <zacharyjparris@gmail.com>
2016-08-07 15:45:59 -0500

    Started build on class and tests for Fibonacci

---
+++ Mathmagician.Tests/FibonacciTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Mathmagician.Commands;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mathmagician.Tests
+{
+    [TestClass]
+    public class FibonacciTests
+    {
+        [TestMethod]
+        public void FibonacciCanBeInstantiated()
+        {
+            Fibonacci test = new Fibonacci();
+            Assert.IsNotNull(test);
+        }
+    }
+}
--- Mathmagician/Commands/Fibonacci.cs
+++ Mathmagician/Commands/Fibonacci.cs
@@ -6,11 +6,23 @@ using System.Threading.Tasks;
 
 namespace Mathmagician.Commands
 {
-    class Fibonacci : Command
+    public class Fibonacci : Command
     {
         public override void CreateNumbers(int quantityOfNumsToCreate)
         {
-
+            int firstInt = 0, secondInt = 1, next;
+            for (int i = 0; Numbers.Count != quantityOfNumsToCreate; i++)
+            {
+                if (i <= 1)
+                    next = i;
+                else
+                {
+                    next = firstInt + secondInt;
+                    firstInt = secondInt;
+                    secondInt = next; 
+                }
+            }
         }
     }
 }
+//0, 1, 1, 2, 3, 5, 8... 
